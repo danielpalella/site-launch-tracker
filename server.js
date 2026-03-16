2c3deca3b1e104b57810e6132a9ed8817d1af0ac
Daniel Palella <daniel.palella@realworklabs.com>
2026-03-16 16:54:43 -0500

    Add Gmail scope to login OAuth so Gmail threads work after re-login

--- server.js
+++ server.js
@@ -171,7 +171,7 @@ app.get('/auth/google', (_req, res) => {
     client_id:     GOOGLE_CLIENT_ID,
     redirect_uri:  GOOGLE_REDIRECT_URI,
     response_type: 'code',
-    scope:         'openid email profile',
+    scope:         'openid email profile https://www.googleapis.com/auth/gmail.readonly',
     access_type:   'offline',
     prompt:        'consent',
     state:         createState(),
